6065badc901ed489b3ad4d901035643e00418691
dawlau <blahoviciandrei1@gmail.com>
2022-03-13 23:53:17 +0200

    image processing baseline

--- agent_training/DataLoader.py
+++ agent_training/DataLoader.py
@@ -1,15 +1,11 @@
 import os
 import numpy as np
 import tensorflow as tf
-from typing import Generator, Dict, Iterable
+from typing import Generator, Dict, Iterable, Tuple
 from Rasterizer import Rasterizer
 
 roadgraph_features = {
-	'roadgraph_samples/id':
-		tf.io.FixedLenFeature([20000, 1], tf.int64, default_value=None),
 	'roadgraph_samples/type':
-		tf.io.FixedLenFeature([20000, 1], tf.int64, default_value=None),
-	'roadgraph_samples/valid':
 		tf.io.FixedLenFeature([20000, 1], tf.int64, default_value=None),
 	'roadgraph_samples/xyz':
 		tf.io.FixedLenFeature([20000, 3], tf.float32, default_value=None),
@@ -17,40 +13,16 @@ roadgraph_features = {
 
 # Features of agents.
 state_features = {
-	'state/id':
-		tf.io.FixedLenFeature([128], tf.float32, default_value=None),
 	'state/type':
 		tf.io.FixedLenFeature([128], tf.float32, default_value=None),
-	'state/is_sdc':
-		tf.io.FixedLenFeature([128], tf.int64, default_value=None),
-	"state/current/velocity_x":
-		tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
-	"state/current/velocity_y":
-		tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
 	'state/current/bbox_yaw':
-		tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
-	'state/current/length':
 		tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
 	'state/current/valid':
 		tf.io.FixedLenFeature([128, 1], tf.int64, default_value=None),
-	'state/current/width':
-		tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
 	'state/current/x':
 		tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
 	'state/current/y':
 		tf.io.FixedLenFeature([128, 1], tf.float32, default_value=None),
-	"state/future/velocity_x":
-		tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
-	"state/future/velocity_y":
-		tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
-	'state/future/bbox_yaw':
-		tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
-	'state/future/length':
-		tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
-	'state/future/valid':
-		tf.io.FixedLenFeature([128, 80], tf.int64, default_value=None),
-	'state/future/width':
-		tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
 	'state/future/x':
 		tf.io.FixedLenFeature([128, 80], tf.float32, default_value=None),
 	'state/future/y':
@@ -61,12 +33,8 @@ state_features = {
 		tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
 	'state/past/bbox_yaw':
 		tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
-	'state/past/length':
-		tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
 	'state/past/valid':
 		tf.io.FixedLenFeature([128, 10], tf.int64, default_value=None),
-	'state/past/width':
-		tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
 	'state/past/x':
 		tf.io.FixedLenFeature([128, 10], tf.float32, default_value=None),
 	'state/past/y':
@@ -78,31 +46,17 @@ state_features = {
 traffic_light_features = {
 	'traffic_light_state/current/state':
 		tf.io.FixedLenFeature([1, 16], tf.int64, default_value=None),
-	'traffic_light_state/current/valid':
-		tf.io.FixedLenFeature([1, 16], tf.int64, default_value=None),
 	'traffic_light_state/current/x':
 		tf.io.FixedLenFeature([1, 16], tf.float32, default_value=None),
 	'traffic_light_state/current/y':
 		tf.io.FixedLenFeature([1, 16], tf.float32, default_value=None),
-	'traffic_light_state/past/state':
-		tf.io.FixedLenFeature([10, 16], tf.int64, default_value=None),
-	'traffic_light_state/past/valid':
-		tf.io.FixedLenFeature([10, 16], tf.int64, default_value=None),
-	'traffic_light_state/past/x':
-		tf.io.FixedLenFeature([10, 16], tf.float32, default_value=None),
-	'traffic_light_state/past/y':
-		tf.io.FixedLenFeature([10, 16], tf.float32, default_value=None),
-	'traffic_light_state/future/state':
-		tf.io.FixedLenFeature([80, 16], tf.int64, default_value=None),
-	'traffic_light_state/future/valid':
-		tf.io.FixedLenFeature([80, 16], tf.int64, default_value=None),
-	'traffic_light_state/future/x':
-		tf.io.FixedLenFeature([80, 16], tf.float32, default_value=None),
-	'traffic_light_state/future/y':
-		tf.io.FixedLenFeature([80, 16], tf.float32, default_value=None),
 }
 
+DISTANCE_THRESHOLD = 30.0
+SURROUNDING_AGENTS_NO = 10
+
 class DataLoader:
+
 
 	@classmethod
 	def __init__(self, data_path: str) -> None:
@@ -131,11 +85,101 @@ class DataLoader:
 			"state/future/y"
 		]
 
+
 	@staticmethod
 	def get_agents_features(data: Dict, features: Iterable[str]) -> tf.Tensor:
 		agent_features = [data[feature] for feature in features]
 		agent_features = tf.stack(agent_features, -1)
 		return agent_features
+
+
+	@classmethod
+	def parse_scenario(self,
+		data: Dict) -> Tuple[
+			tf.Tensor, tf.Tensor, tf.Tensor,
+			tf.Tensor, tf.Tensor, tf.Tensor, tf.Tensor]:
+
+		past_is_valid = data["state/past/valid"] > 0
+		current_is_valid = data["state/current/valid"] > 0
+
+		valid_agents_mask = tf.reduce_any(
+			tf.concat([past_is_valid, current_is_valid], 1), 1)
+
+		agents_to_predict = data["state/tracks_to_predict"] > 0
+		agent_types = data["state/type"][valid_agents_mask]
+
+		past_agents = DataLoader.get_agents_features(data, self.agent_past_features)[valid_agents_mask]
+		current_agents = DataLoader.get_agents_features(data, self.agent_current_features)[valid_agents_mask]
+		future_agents = DataLoader.get_agents_features(data, self.agent_future_features)[valid_agents_mask]
+
+		roadgraph_coords = data["roadgraph_samples/xyz"]
+		roadgraph_type = tf.cast(data["roadgraph_samples/type"], dtype=tf.float32)
+		roadgraph = tf.concat(
+			[roadgraph_coords, roadgraph_type], -1)
+		roadgraph_mask = roadgraph_type > 0
+		roadgraph = roadgraph[tf.squeeze(roadgraph_mask)]
+
+		traffic_lights_x = tf.transpose(data["traffic_light_state/current/x"])
+		traffic_lights_y = tf.transpose(data["traffic_light_state/current/y"])
+		traffic_lights_state = tf.transpose(data["traffic_light_state/current/state"])
+		traffic_lights_mask = traffic_lights_state > 0
+		traffic_lights_state = tf.cast(traffic_lights_state, dtype=tf.float32)
+		traffic_lights = tf.concat(
+			[traffic_lights_x, traffic_lights_y, traffic_lights_state], 1)
+		traffic_lights = traffic_lights[tf.squeeze(traffic_lights_mask)]
+
+		return past_agents, current_agents, agents_to_predict, agent_types, roadgraph, traffic_lights, future_agents
+
+
+	@classmethod
+	def get_surrounding_agents(self,
+		past_surrounding_agents: tf.Tensor,
+		current_surrounding_agents: tf.Tensor,
+		current_target_agent: tf.Tensor,
+		surrounding_agents_types: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
+
+		# prepare features for distance filtering
+		# remove heading angle
+		target_agent_xy 	  = current_target_agent[..., : -1]
+		surrounding_agents_xy = current_surrounding_agents[..., : -1]
+
+		# reshape such that new tensor is of shape (agents_no, 2)
+		target_agent_xy = tf.reshape(
+			target_agent_xy, shape=(target_agent_xy.shape[0], target_agent_xy.shape[-1]))
+
+		surrounding_agents_xy = tf.reshape(
+			surrounding_agents_xy, shape=(surrounding_agents_xy.shape[0], surrounding_agents_xy.shape[-1]))
+
+		# filter agents that are farther from the target agent than the threshold
+		distances = tf.norm(
+			tf.math.subtract(surrounding_agents_xy, target_agent_xy), axis=-1)
+
+		# remove agents that are farther than needed
+		distances_mask 			   = distances <= DISTANCE_THRESHOLD
+		past_surrounding_agents    = past_surrounding_agents[distances_mask]
+		current_surrounding_agents = current_surrounding_agents[distances_mask]
+		surrounding_agents_xy 	   = surrounding_agents_xy[distances_mask]
+
+		# calculate distances of relevant agents and get the closest ones
+		distances = tf.norm(
+			tf.math.subtract(surrounding_agents_xy, target_agent_xy), axis=-1)
+
+		best_agents = tf.math.top_k(
+			-distances, k=min(SURROUNDING_AGENTS_NO, distances.shape[-1])).indices
+
+		# these are going to be the relevant surrounding agents
+		past_surrounding_agents = tf.gather(
+			past_surrounding_agents, best_agents)
+		current_surrounding_agents = tf.gather(
+			current_surrounding_agents, best_agents)
+
+		# remove the farthest ones
+		surrounding_agents_types = surrounding_agents_types[distances_mask]
+		# get the closest ones
+		surrounding_agents_types = tf.gather(
+			surrounding_agents_types, best_agents)
+
+		return past_surrounding_agents, current_surrounding_agents, surrounding_agents_types
 
 
 	@classmethod
@@ -147,76 +191,50 @@ class DataLoader:
 			for scenario in scenarios.as_numpy_iterator():
 				data = tf.io.parse_single_example(scenario, self.features_description)
 
-				past_is_valid = data["state/past/valid"] > 0
-				current_is_valid = data["state/current/valid"] > 0
-
-				valid_agents_mask = tf.reduce_any(
-					tf.concat([past_is_valid, current_is_valid], 1), 1)
-
-				agents_to_predict = data["state/tracks_to_predict"] > 0
-
-				past_agents = DataLoader.get_agents_features(data, self.agent_past_features)[valid_agents_mask]
-				current_agents = DataLoader.get_agents_features(data, self.agent_current_features)[valid_agents_mask]
-				future_agents = DataLoader.get_agents_features(data, self.agent_future_features)[valid_agents_mask]
-
-				roadgraph_coords = data["roadgraph_samples/xyz"]
-				roadgraph_type = tf.cast(data["roadgraph_samples/type"], dtype=tf.float32)
-				roadgraph = tf.concat(
-					[roadgraph_coords, roadgraph_type], -1)
-				roadgraph_mask = roadgraph_type > 0
-				roadgraph = roadgraph[tf.squeeze(roadgraph_mask)]
-
-				traffic_lights_x = tf.transpose(data["traffic_light_state/current/x"])
-				traffic_lights_y = tf.transpose(data["traffic_light_state/current/y"])
-				traffic_lights_state = tf.transpose(data["traffic_light_state/current/state"])
-				traffic_lights_mask = traffic_lights_state > 0
-				traffic_lights_state = tf.cast(traffic_lights_state, dtype=tf.float32)
-				traffic_lights = tf.concat(
-					[traffic_lights_x, traffic_lights_y, traffic_lights_state], 1)
-				traffic_lights = traffic_lights[tf.squeeze(traffic_lights_mask)]
+				past_agents, current_agents, agents_to_predict, agent_types, roadgraph, traffic_lights, future_agents = self.parse_scenario(data)
 
 				for x in tf.where(agents_to_predict):
 					idx = tf.squeeze(x)
 
-					past_target_agent = past_agents[idx]
+					# features of target agent
+					past_target_agent 	 = past_agents[idx]
 					current_target_agent = current_agents[idx]
 
-					past_surrounding_agents = tf.concat(
+					# features of surrounding agents
+					past_surrounding_agents    = tf.concat(
 						[past_agents[ : idx], past_agents[idx + 1 : ]], 0)
 					current_surrounding_agents = tf.concat(
 						[current_agents[ : idx], current_agents[idx + 1 : ]], 0)
+					surrounding_agents_types   = tf.concat(
+						[agent_types[ : idx], agent_types[idx + 1 : ]], 0)
 
-					target_agent_xy = tf.squeeze(current_target_agent[..., : -1])
-					surrounding_agents_xy = tf.squeeze(current_surrounding_agents[..., : -1])
-
-					distances = tf.norm(
-						tf.math.subtract(surrounding_agents_xy, target_agent_xy), axis=-1)
-
-					distances = tf.reshape(distances, shape=(-1, ))
-					best_agents = tf.math.top_k(distances, k=min(10, distances.shape[-1])).indices
-
-					past_surrounding_agents = tf.gather(
-						past_surrounding_agents, best_agents)
-					current_surrounding_agents = tf.gather(
-						current_surrounding_agents, best_agents)
-
-					surrounding_agents = past_surrounding_agents
-					env_context_surrounding_agents = tf.concat(
-						[past_surrounding_agents[..., : 3], current_surrounding_agents], axis=1)
+					past_surrounding_agents, current_surrounding_agents, surrounding_agents_types = self.get_surrounding_agents(past_surrounding_agents, current_surrounding_agents, current_target_agent, surrounding_agents_types)
 
 					target_agent = past_target_agent
+					# padding surrounding agents with zeros
+					padding_size = SURROUNDING_AGENTS_NO - past_surrounding_agents.shape[0]
+					surrounding_agents = tf.concat([
+						past_surrounding_agents,
+						tf.zeros((padding_size,) + past_surrounding_agents.shape[1 : ])], 0)
+
+					# prepare data for environment context rasterizer
+					# remove velocities and concat with current timestamp for surrounding agents
+					env_context_surrounding_agents = tf.concat(
+						[past_surrounding_agents[..., : 3], current_surrounding_agents], 1)
+
 					env_context_target_agent = tf.concat(
 						[past_target_agent[..., : 3], current_target_agent], 0)
-
 
 					environment_context = Rasterizer.rasterize(
 						roadgraph,
 						traffic_lights,
 						env_context_target_agent,
-						env_context_surrounding_agents
+						env_context_surrounding_agents,
+						surrounding_agents_types
 					)
 
 					# yield environment_context, target_agent, surrounding_agents
+					# break
 
 
-				# break
+				break
--- agent_training/Rasterizer.py
+++ agent_training/Rasterizer.py
@@ -1,47 +1,95 @@
 import tensorflow as tf
 import numpy as np
 import sys
+from matplotlib import pyplot as plt
+
 
 class Rasterizer:
 
-	image_width = 250
-	image_height = 250
+	IMAGE_WIDTH = 240
+	IMAGE_HEIGHT = 240
+	TARGET_AGENT_COLOR = (255, 0, 0) # red
+	BACKGROUND_COLOR = (0, 0, 0) # black
+	TARGET_AGENT_CENTER = (IMAGE_WIDTH / 5,	IMAGE_HEIGHT / 2)
+	SURROUNDING_AGENTS_COLORS = [
+		(255, 0, 255), # magenta
+		(0, 0, 255), # blue
+		(0, 255, 0) # green
+	]
 
 	@staticmethod
-	def get_boundaries(roadgraph, traffic_lights, agents):
-		x_min = sys.float_info.max
-		y_min = sys.float_info.max
-		x_max = -sys.float_info.max
-		y_max = -sys.float_info.max
-
-		roads = [road.get_polyline() for road in roadgraph]
-		for road in roads:
-			for vertex in road:
-				x, y = vertex
-
-				x_min = min(x_min, x)
-				x_max = max(x_max, x)
-				y_min = min(y_min, y)
-				y_max = max(y_max, y)
-
-		return x_min, y_min, x_max, y_max
+	def show_image(environment_context: np.array) -> None:
+		environment_context = environment_context.transpose((1, 0, 2))
+		plt.imshow(environment_context, origin="lower")
+		plt.show()
 
 	@staticmethod
-	def get_normalized_coordinates(vertex, x_min, y_min, x_max, y_max):
-		x, y = vertex
-		real_width = x_max - x_min
-		real_height = y_max - y_min
+	def rasterize(
+		roadgraph: tf.Tensor,
+		traffic_lights: tf.Tensor,
+		target_agent: tf.Tensor,
+		surrounding_agents: tf.Tensor,
+		surrounding_agents_types: tf.Tensor) -> tf.Tensor:
 
-		x = (x - x_min - real_width / 2) * Rasterizer.image_width / real_width + Rasterizer.image_width / 2
-		y = (y - y_min - real_height / 2) * Rasterizer.image_height / real_height + Rasterizer.image_height / 2
+		"""
+			document the function
+			add function annotations
+			Notes:
+				- delete invalid coords for agents
+				- filter for points in boundaries
+				- rotations
+				- bboxes
+				- circles as traffic lights
+				- how the fuck do I do streets?
+				- continuous to discrete
+		"""
 
-		return x, y
+		environment_context = np.full(
+			shape=(Rasterizer.IMAGE_WIDTH, Rasterizer.IMAGE_HEIGHT, 3),
+			fill_value=Rasterizer.BACKGROUND_COLOR)
 
-	@staticmethod
-	def rasterize(roadgraph, traffic_lights, target_agent, surrounding_agents):
-		pass
+		past_surrounding_agents    = surrounding_agents[ : , : -1, : ]
+		current_surrounding_agents = surrounding_agents[-1]
+
+		past_target_agent 	 = target_agent[ : -1]
+		current_target_agent = target_agent[-1]
+
+		translation = tf.math.subtract(
+			Rasterizer.TARGET_AGENT_CENTER, current_target_agent[ : -1])
+
+
+		# draw tails
+
+		past_surrounding_agents_xy = past_surrounding_agents[..., : -1]
+		past_target_agent_xy 	   = past_target_agent[..., : -1]
+
+		past_surrounding_agents_xy += translation
+		past_target_agent_xy 	   += translation
+
+		past_surrounding_agents_xy = tf.cast(
+			past_surrounding_agents_xy, dtype=tf.int32)
+		past_target_agent_xy	   = tf.cast(
+			past_target_agent_xy, dtype=tf.int32)
+
+		environment_context[tuple(tf.transpose(past_target_agent_xy))] = Rasterizer.TARGET_AGENT_COLOR
+
+		for i, x in enumerate(past_surrounding_agents_xy):
+			agent_type = surrounding_agents_types[i] - 1
+			agent_type = int(agent_type)
+			environment_context[tuple(tf.transpose(x))] = Rasterizer.SURROUNDING_AGENTS_COLORS[agent_type]
+
+		Rasterizer.show_image(environment_context)
+
+		# print(target_agent_theta)
+		# print(roadgraph.shape)
+		# print(traffic_lights.shape)
 		# print(target_agent.shape)
-		# x_min, y_min, x_max, y_max = Rasterizer.get_boundaries(roadgraph, traffic_lights, agents)
+		# print(surrounding_agents.shape)
+		# print(surrounding_agents_types.shape)
+		# print("------------------------------------------")
+
+		# print(target_agent.shape)
+		# x_min, y_min, x_mplt.axis(), y_max = Rasterizer.get_boundaries(roadgraph, traffic_lights, agents)
 
 		# image = np.array(np.zeros(shape = (Rasterizer.image_width + 1, Rasterizer.image_height + 1, 3)))
 		# for i in range(Rasterizer.image_width):
@@ -50,13 +98,13 @@ class Rasterizer:
 
 		# for road in roadgraph:
 		# 	for vertex in road.get_polyline():
-		# 		x, y = Rasterizer.get_normalized_coordinates(vertex, x_min, y_min, x_max, y_max)
+		# 		x, y = Rasterizer.get_normalized_coordinates(vertex, x_min, y_min, x_mplt.axis(), y_max)
 		# 		x = int(x)
 		# 		y = int(y)
 		# 		image[x][y] = road.get_color()
 
 		# for traffic_light in traffic_lights:
-		# 	x, y = Rasterizer.get_normalized_coordinates((traffic_light.get_x(), traffic_light.get_y()), x_min, y_min, x_max, y_max)
+		# 	x, y = Rasterizer.get_normalized_coordinates((traffic_light.get_x(), traffic_light.get_y()), x_min, y_min, x_mplt.axis(), y_max)
 		# 	x = int(x)
 		# 	y = int(y)
 		# 	for i in range(x - 1, x + 1):
@@ -66,7 +114,7 @@ class Rasterizer:
 		# for agent in agents:
 		# 	vertices = agent.get_bbox()
 		# 	for vertex in vertices:
-		# 		x, y = Rasterizer.get_normalized_coordinates(vertex, x_min, y_min, x_max, y_max)
+		# 		x, y = Rasterizer.get_normalized_coordinates(vertex, x_min, y_min, x_mplt.axis(), y_max)
 
 		# 		x = int(x)
 		# 		y = int(y)
